fb007414616b797b1f83b321c9240128d4b0b534
Mehrajul Islam <you@example.com>
2023-07-23 01:05:16 +0600

    added all event listeners in the login js file

--- login/login.js
+++ login/login.js
@@ -18,3 +18,107 @@ let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
 
 var token;
+
+tasksContainer.addEventListener('click', e => {
+  if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find(list => list.id === selectedListId)
+    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+    selectedTask.complete = e.target.checked
+    save()
+    renderTaskCount(selectedList)
+  }
+})
+
+clearCompleteTasksButton.addEventListener('click', e => {
+    window.location.href = "../signup/signup.html";
+  saveAndRender()
+})
+
+deleteListButton.addEventListener('click', e => {
+  console.log('forget it')
+})
+
+document.getElementById('login-form').addEventListener('submit', e => {
+  e.preventDefault()
+  login()
+  saveAndRender()
+})
+
+loginSubmitButton.addEventListener('click', e => {
+  e.preventDefault()
+  login();
+})
+
+function login() {
+  const entredLoginCredentials = {
+    username: document.getElementById('login-username').value,
+    password: document.getElementById('login-password').value
+  };  
+  fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify(entredLoginCredentials)
+  }).then(response => response.json())
+    .then(data => {      
+      token = data.token;
+      if(token) {
+        console.log('token that you got : ',token);
+        localStorage.setItem('token', token);
+        localStorage.setItem('username', entredLoginCredentials.username);
+        window.location.href = "../home/home.html";
+      }
+      else {
+        alert('Invalid credentials')
+      }      
+    })
+    .catch(error => {
+      console.error('Error : ', error);
+      alert("Server error")
+    });
+}
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function createTask(name) {
+  return { id: Date.now().toString(), name: name, complete: false }
+}
+
+function saveAndRender() {
+  save()
+  render()
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+function render() {
+  //clearElement(listsContainer)
+  renderLists()
+
+  const selectedList = lists.find(list => list.id === selectedListId)
+  if (selectedListId == null) {
+    listDisplayContainer.style.display = 'none'
+  } else {
+    listDisplayContainer.style.display = ''
+  }
+}
+
+function renderLists() {
+  lists.forEach(list => {
+    const listElement = document.createElement('li')
+    listElement.dataset.listId = list.id
+    listElement.classList.add("list-name")
+    listElement.innerText = list.name
+    if (list.id === selectedListId) {
+      listElement.classList.add('active-list')
+    }
+  })
+}
+
+render()
