6144687a37b2c4029d15ff4fec473dd742b6a6ef
Mehrajul Islam <you@example.com>
2023-07-23 18:50:16 +0600

    restructured and refactored unneccesary codes in signup js

--- signup/signup.js
+++ signup/signup.js
@@ -1,123 +1,50 @@
-//const { response } = require("express")
+// Document queries and vars
 
-const listsContainer = document.querySelector('[data-lists]')
-const newListForm = document.querySelector('[data-new-list-form]')
-const newListInput = document.querySelector('[data-new-list-input]')
-const deleteListButton = document.querySelector('[data-delete-list-button]')
-const listDisplayContainer = document.querySelector('[data-list-display-container]')
-const listTitleElement = document.querySelector('[data-list-title]')
-const listCountElement = document.querySelector('[data-list-count]')
-const tasksContainer = document.querySelector('[data-tasks]')
-const taskTemplate = document.getElementById('task-template')
-const newTaskForm = document.querySelector('[data-new-task-form]')
-const newTaskInput = document.querySelector('[data-new-task-input]')
-const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
+const signupForm = document.getElementById('signup-form');
+const loginButton = document.querySelector('[data-clear-complete-tasks-button]')
 const signupSubmitButton = document.querySelector('[auth-signup-submit]')
-const defaultPath = '/references/todo-list-collab/javascript-finished'
-
-const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
-let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
-let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+const forgetPasswordButton = document.querySelector('[data-delete-list-button]')
 
 var token;
 
-tasksContainer.addEventListener('click', e => {
-  if (e.target.tagName.toLowerCase() === 'input') {
-    const selectedList = lists.find(list => list.id === selectedListId)
-    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
-    selectedTask.complete = e.target.checked
-    save()
-    renderTaskCount(selectedList)
-  }
+// Event Listeners
+
+loginButton.addEventListener('click', e => {
+  window.location.href = "../login/login.html";
 })
 
-clearCompleteTasksButton.addEventListener('click', e => {
-    window.location.href =  "../login/login.html";
-  saveAndRender()
+forgetPasswordButton.addEventListener('click', e => {
+  alert('not available yet')
 })
 
-deleteListButton.addEventListener('click', e => {
-  console.log('forget it')
-})
+signupForm.addEventListener('submit', e => signup(e));
+signupSubmitButton.addEventListener('click', e => signup(e));
 
-document.getElementById('signup-form').addEventListener('submit', e => {
-  e.preventDefault()
-  //const taskName = newTaskInput.value
-  //if (taskName == null || taskName === '') return
-  //const task = createTask(taskName)
-  //newTaskInput.value = null
-  //const selectedList = lists.find(list => list.id === selectedListId)
-  //selectedList.tasks.push(task)
-  signup();
-  saveAndRender()
-})
-
-signupSubmitButton.addEventListener('click', e => {
-  e.preventDefault()
-  signup();
-})
-
-function signup(){
+function signup(e) {
+  e.preventDefault();
   const entredSignupCredentials = {
     username: document.getElementById('signup-username').value,
     password: document.getElementById('signup-password').value
   };
   fetch('http://localhost:3000/signup', {
     method: 'POST',
-    headers:{
+    headers: {
       'Content-Type': 'application/json'
-    }, 
+    },
     body: JSON.stringify(entredSignupCredentials)
   }).then(response => response.json())
-    .then(data => {      
+    .then(data => {
       token = data.token;
-      if(token) {
-        console.log('token that you got : ',token);
-        localStorage.setItem('token', token);
+      if (token) {
+        console.log('token that you got : ', token);
+        alert('Account Created Successfully');
         window.location.href = "../login/login.html";
       }
       else {
         alert(data.message)
-      }      
+      }
     })
     .catch(error => {
-      console.error('Error : ', error);
+      alert('Server Error')
     });
 }
-
-function createList(name) {
-  return { id: Date.now().toString(), name: name, tasks: [] }
-}
-
-function createTask(name) {
-  return { id: Date.now().toString(), name: name, complete: false }
-}
-
-function saveAndRender() {
-  save()
-  render()
-}
-
-function save() {
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
-  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
-}
-
-function render() {
-  //clearElement(listsContainer)
-  //renderLists()
-
-  const selectedList = lists.find(list => list.id === selectedListId)
-  if (selectedListId == null) {
-    listDisplayContainer.style.display = 'none'
-  } else {
-    listDisplayContainer.style.display = ''
-    //listTitleElement.innerText = selectedList.name
-    //renderTaskCount(selectedList)
-    //clearElement(tasksContainer)
-    //renderTasks(selectedList)
-  }
-}
-
-render()
